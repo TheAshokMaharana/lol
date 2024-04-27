3762f4f3a73002cbe54f66cd02e2c429cf60733e
SDAC-8 <SDAC-8@SDAC-8-PC>
2024-04-27 10:19:46 -0700

    v0.0.3

--- src/helloworld/secondhello.java
+++ src/helloworld/secondhello.java
@@ -3,7 +3,7 @@ package helloworld;
 public class secondhello {
 	public static void main(String[] args) {
 		System.out.println("google v1" );
-		System.out.println("google v1" );
+		System.out.println("google v2" );
 	}
 
 }
